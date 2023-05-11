6cc755ac2f8416ad4a28167853d091515199dd10
RrLSn <afolabisodiqtemitope@yahoo.com>
2023-05-11 23:49:51 +0100

    translator App

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -6,20 +6,74 @@ const Translator = () => {
 
     const [inputText, setInputText] = useState('')
     const [outputText, setOutputText] = useState('')
-    console.log(inputText)
+
+    // const url = 'https://google-translator9.p.rapidapi.com/v2'
+
+    
+    const translateClick = async(e) =>{
+        e.preventDefault()
+        // const options = {
+        //     method: 'POST',
+        //     headers: {
+        //         'content-type': 'application/json',
+        //         'X-RapidAPI-Key': '92a22776b5msh423d8dbc585bf21p1fd0c4jsn109e0f8add59',
+        //         'X-RapidAPI-Host': 'google-translator9.p.rapidapi.com'
+        //     },
+        //     body: {
+        //         q: 'The Great Pyramid of Giza (also known as the Pyramid of Khufu or the Pyramid of Cheops) is the oldest and largest of the three pyramids in the Giza pyramid complex.',
+        //         source: 'en',
+        //         target: 'ar',
+        //         format: 'text'
+        //     }
+        // };
+        // fetch(url,options)
+        // .then((res) => res.json())
+        // .then((data) => console.log(data))
+        // .catch((err) => console.log(err))
+        // try {
+        //     const response = await fetch(url, options);
+        //     // const result = await response.text();
+        //     console.log(result);
+        // } catch (error) {
+        //     console.error(error);
+        // }
+        const url = 'https://google-translator9.p.rapidapi.com/v2';
+        const options = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': 'e3edb7f51amshef58cd9a59107bcp1b3fd0jsna96710f87208',
+            'X-RapidAPI-Host': 'google-translator9.p.rapidapi.com'
+        },
+        body: {
+            q: 'The Great Pyramid of Giza (also known as the Pyramid of Khufu or the Pyramid of Cheops) is the oldest and largest of the three pyramids in the Giza pyramid complex.',
+            source: 'en',
+            target: 'zh-CN',
+            format: 'text'
+        }
+        };
+
+        try {
+            const response = await fetch(url, options);
+            const result = await response.text();
+            console.log(result);
+        } catch (error) {
+            // console.error(error);
+        }
+    }
 
   return (
     <div>
         <main>
             <div className="inputTextContainer containers">
-                <div className='btn'>
+                <form onSubmit={translateClick} className='btn'>
                 <button className='translateBtn'>Translate</button>
-                </div>
-                <form action="">
+                </form>
+                <div action="">
                     <textarea 
                     placeholder='Enter Text (any Language)' className='resize-none p-[1rem]'
                     onChange={(e) => setInputText(e.target.value)}></textarea>
-                </form>
+                </div>
             </div>
             <div className="outputTextContainer containers">
                 <Languages />
